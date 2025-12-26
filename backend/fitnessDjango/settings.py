c9a2ce42a3a93a0bfea1c0c82f7d69b7fc0a2be4
subav11 <subhamyadav7528@gmail.com>
2025-12-27 00:42:01 +0530

    updated build.sh

--- backend/fitnessDjango/settings.py
+++ backend/fitnessDjango/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=env("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=not DEBUG,
     )
 }
 
